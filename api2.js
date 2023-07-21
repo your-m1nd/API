e3caae750bb389a253848bd9c59564814ecb9c30
your-m1nd <vi.neurogirl@gmail.com>
2023-07-21 20:45:24 +0300

    change api2.js

--- api2.js
+++ api2.js
@@ -15,21 +15,21 @@ createBtn.onclick = function (event) {
      h2.textContent = inputTitle.value;
      paragraf.textContent = inputText.value;
 
-     let post = {
-        title: document.getElementById('inputTitle').value,
-        body: document.getElementById('inputText').value,
-     }
-
+     let title = inputTitle.value;
+     let body = inputText.value;
 
      fetch('https://jsonplaceholder.typicode.com/posts', {
         method: 'POST',
-        body: JSON.stringify(post),
+        body: JSON.stringify({
+          title: `${title}`,
+          body: `${body}`,
+        }),
         headers: {
           'Content-type': 'application/json; charset=UTF-8',
         },
       })
         .then((response) => response.json())
-        .then((post) => console.log(post),
+        .then((json) => console.log(json),
      )}
 
 
