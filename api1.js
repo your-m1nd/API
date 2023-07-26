ffb9d3b92d6c38a7d8594ee291c20553640ca0f9
your-m1nd <vi.neurogirl@gmail.com>
2023-07-26 16:37:16 +0300

    working on mistakes

--- api1.js
+++ api1.js
@@ -1,6 +1,6 @@
 //получаем от сервера список постов и отображаем на странице
 
-document.addEventListener("DOMContentLoaded", function (event) {
+/*document.addEventListener("DOMContentLoaded", function (event) {
     fetch("https://jsonplaceholder.typicode.com/posts")
     .then(response => response.json())
     .then(json => json.forEach(element => {
@@ -18,6 +18,32 @@ document.addEventListener("DOMContentLoaded", function (event) {
     .catch(error => console.log(error));    
 }
 )
+*/
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response => response.json())
+    .then(json => {
+        json.forEach(element => {
+          displayPost(element);
+        });
+      })
+      .catch(error => console.log(error));
+  });
+
+  function displayPost(post) {
+    let article = document.createElement('article');
+    let title = document.createElement('h2');
+    let paragraf = document.createElement('p');
+
+  article.appendChild(title);
+  article.appendChild(paragraf);
+  title.textContent = `Заголовок: ${post.title}`;
+  paragraf.textContent = `Статья: ${post.body}`;
+
+  let container = document.querySelector('.post-container');
+  container.appendChild(article);
+  }
 
 
 
